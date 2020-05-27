9c56fffe59b5adfe7a9b0e79dbe678a4c9973d72
PARDEEP KAUR <pkaur84@aol.com>
2020-05-27 16:41:16 +0000

    Automatically backed up by Learn

---
+++ lib/cat.rb
@@ -0,0 +1,14 @@
+class Cat
+attr_accessor  :owner, :mood
+attr_reader :name
+  @@all =[]
+  def initialize (name, owner)
+    @owner =owner
+    @name = name
+    @mood = "nervous"
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+end
