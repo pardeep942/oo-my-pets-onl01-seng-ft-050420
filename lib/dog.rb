2be6fa23ceb39fc7dede9cabe49fe250b70bd43f
PARDEEP KAUR <pkaur84@aol.com>
2020-05-27 16:27:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,2 @@
 class Dog
-  attr_accessor :owner, :mood
-  attr_reader :name
-
-  @@all = [ ]
-
-def initialize(name, owner)
-      @name = name
-      @owner = owner
-      @@all << self 
-      @owner.pets=(self)
-      @mood = "nervous"
-  end
-
-  def self.all
-      @@all
-  end
-end 
+  
